8b832c3c0a4edce6497b39988f3f80347944fa1e
Nick Kvizhinadze <nickkvizhinadze@hotmail.com>
2024-02-22 22:36:03 +0400

    Used ms library for calculating miliseconds

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from './usePlatforms';
 import ApiClient, { FetchResponse } from "../services/api-client";
+import ms from "ms";
 
 export interface Game {
     id: number;
@@ -20,7 +21,7 @@ const useGames = (gameQuery: GameQuery) =>
             params:
             {
                 genres: gameQuery.genreId,
-                platforms: gameQuery.platformId?.id,
+                platforms: gameQuery.platformId,
                 ordering: gameQuery.sortOrder,
                 search: gameQuery.searchText,
                 page: pageParam
@@ -29,7 +30,7 @@ const useGames = (gameQuery: GameQuery) =>
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined;
         },
-        staleTime: 25 * 60 * 60 * 1000
+        staleTime:  ms("24h"),
     });
 
 export default useGames;
