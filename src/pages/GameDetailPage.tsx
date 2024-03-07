7a886ed96775de56a1ad449fe4886dbd5debf9dd
Nick Kvizhinadze <nickkvizhinadze@hotmail.com>
2024-03-07 11:31:27 +0400

    Added grid to game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import Expandable from "../components/Expandable";
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
   if (error) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <Expandable>{game?.description_raw}</Expandable>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameSchreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <Expandable>{game?.description_raw}</Expandable>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameSchreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
